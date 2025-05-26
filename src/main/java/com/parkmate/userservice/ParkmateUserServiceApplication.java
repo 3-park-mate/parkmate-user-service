258a78b5a7c21c06c64ce89bd5c258617fedec78
DoNalD-A <thdlstjrj@naver.com>
2025-05-26 09:47:42 +0900

    feat: user-service init

---
+++ src/main/java/com/parkmate/userservice/ParkmateUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.parkmate.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkmateUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkmateUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parkmate/userservice/ParkmateUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkmate.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkmateUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
